afc6f602b33114481b99f4a101dbc0d8c2547bd4
Semmiverian <semmivp1@gmail.com>
2019-03-30 15:46:25 +0700

    host using .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: 'http://localhost:3001',
-  baseURL: 'http://35.240.131.134/',
+  // baseURL: 'http://35.240.131.134/',
+  baseURL: process.env.REACT_APP_HOST,
 });
